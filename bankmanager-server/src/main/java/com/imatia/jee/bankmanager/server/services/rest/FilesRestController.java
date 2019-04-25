5880c7a3ae5425aa05f05759c6beba62f3225cd7
Rodrigo <rodrigobreton98@gmail.com>
2019-04-25 11:12:42 +0200

    Se preparo la BBDD para guardar las url de los ficheros que subamos

--- bankmanager-server/src/main/java/com/imatia/jee/bankmanager/server/services/rest/FilesRestController.java
+++ bankmanager-server/src/main/java/com/imatia/jee/bankmanager/server/services/rest/FilesRestController.java
@@ -1,5 +1,6 @@
 package com.imatia.jee.bankmanager.server.services.rest;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -20,22 +21,26 @@ import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 public class FilesRestController {
 
 	@PostMapping(value = "upload")
-    public ResponseEntity<String> upload(@RequestParam("name") String[] names, @RequestParam("file") MultipartFile[] files, @RequestParam(name="data", required=false) String data) throws JsonParseException, JsonMappingException, IOException {
+	public ResponseEntity<String> upload(@RequestParam("name") String[] names,
+			@RequestParam("file") MultipartFile[] files, @RequestParam(name = "data", required = false) String data)
+			throws JsonParseException, JsonMappingException, IOException {
 
+		HashMap<String, Object> extraData = new HashMap<>();
 
-		HashMap<String, Object> extraData  = new HashMap<>();
-			
-		if(data!=null) {
+		if (data != null) {
 			extraData = new ObjectMapper().readValue(data, HashMap.class);
 		}
-		
+
 		System.out.println("----- SE RECIBIO EL ARCHIVO -----");
-		
-		for(int i=0;i<files.length;i++) {
+
+		for (int i = 0; i < files.length; i++) {
+			File file = new File(files[i].getOriginalFilename());
+			File path = file.getAbsoluteFile();
 			System.out.println("Nombre original del archivo: " + files[i].getOriginalFilename());
-			System.out.println("Tamaño del archivo: "+ files[i].getSize());
+			System.out.println("Tamaño del archivo: " + files[i].getSize());
 			System.out.println("Numero de archivos: " + files.length);
-		}		
+			System.out.println("Ruta del archivo: " + path);
+		}
 		return ResponseEntity.ok("-----SE SUBIO EL ARCIHVO-----");
 	}
 
